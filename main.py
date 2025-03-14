afd29eff03d04ea7a9e5ebb44a7e51e45ba8bd60
Austin Carr <austincarr@Austins-MacBook-Air-2.local>
2025-03-13 20:13:18 -0400

    Enabled CORS for frontend

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://ac2500.github.io"],  # Allow frontend access
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def home():
     return {"message": "F1 Fantasy Backend is Running!"}
